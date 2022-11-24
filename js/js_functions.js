6695b5e65fd074de6791effd682e6b783ba3d539
Pranali-22 <lpranali22@gmail.com>
2022-11-24 23:57:17 +0530

    Added: UC2 - Validate Email

--- js/js_functions.js
+++ js/js_functions.js
@@ -14,4 +14,16 @@ const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
 salary.addEventListener('input', function(){
     output.textContent = salary.value;
-})
+});
+
+
+//UC2
+const emailId = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+emailId.addEventListener('input',function(){
+    let emailRegex = RegExp('^[a-z]{1,}(.[a-z]{1,})?@bl.co(.in)?$');
+    if(emailRegex.test(emailId.value))
+        emailError.textContent = "";
+    else
+        emailError.textContent = "Invalid Email address";
+});
